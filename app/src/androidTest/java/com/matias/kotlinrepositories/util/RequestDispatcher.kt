25834f0447d9ae92009971153a61a8eb29d45c70
Matias Calvo <25348888+matiascalvo@users.noreply.github.com>
2022-04-14 10:34:35 +0200

    tests: RequestDispatcher - Change numbers for HTTP constants

--- app/src/androidTest/java/com/matias/kotlinrepositories/util/RequestDispatcher.kt
+++ app/src/androidTest/java/com/matias/kotlinrepositories/util/RequestDispatcher.kt
@@ -1,24 +1,27 @@
 package com.matias.kotlinrepositories.util
 
 import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import javax.inject.Inject
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
 
-class RequestDispatcher : Dispatcher() {
-
+class RequestDispatcher @Inject constructor() : Dispatcher() {
     var isEnabled: Boolean = true
 
     override fun dispatch(request: RecordedRequest): MockResponse {
         return when {
-            !isEnabled -> MockResponse().setResponseCode(400)
+            !isEnabled -> MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
             request.path == "/search/repositories?q=language%3Akotlin&page=1&s=stars&per_page=30" ->
-                MockResponse().setResponseCode(200).setBody(getJsonContent("empty_search_page_1.json"))
+                MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                    .setBody(getJsonContent("empty_search_page_1.json"))
             request.path == "/search/repositories?q=language%3Akotlin&page=2&s=stars&per_page=30" ->
-                MockResponse().setResponseCode(200).setBody(getJsonContent("empty_search_page_2.json"))
+                MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                    .setBody(getJsonContent("empty_search_page_2.json"))
             request.path == "/search/repositories?q=test%2Blanguage%3Akotlin&page=0&s=stars&per_page=30" ->
-                MockResponse().setResponseCode(200).setBody(getJsonContent("test_page_1.json"))
-            else -> MockResponse().setResponseCode(400)
+                MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(getJsonContent("test_page_1.json"))
+            else -> MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
         }
     }
 
